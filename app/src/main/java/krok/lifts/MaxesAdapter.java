6ba1b32a87c7771185a5ba7cb3842d76f600966b
Tilman Krokotsch <t.krokotsch@gmail.com>
2016-10-25 12:28:49 +0200

    Added Image to maxes on start screen

--- app/src/main/java/krok/lifts/MaxesAdapter.java
+++ app/src/main/java/krok/lifts/MaxesAdapter.java
@@ -2,10 +2,13 @@ package krok.lifts;
 
 import android.content.Context;
 import android.content.res.Resources;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -17,19 +20,29 @@ import java.util.StringTokenizer;
 public class MaxesAdapter extends android.support.v7.widget.RecyclerView.Adapter<MaxesAdapter.ViewHolder> {
 
     private final String[] mLifts;
+    private final Drawable[] mImages;
 
     public MaxesAdapter(Context context){
         Resources resources = context.getResources();
         mLifts = resources.getStringArray(R.array.compound);
+        TypedArray a = resources.obtainTypedArray(R.array.compoundDrawables);
+        mImages = new Drawable[a.length()];
+        for (int i = 0; i < mImages.length; i++){
+            mImages[i] = a.getDrawable(i);
+        }
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mTextView;
+        public TextView mMaxText;
+        public ImageView mImageView;
 
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.fragment_tile_content, parent, false));
-            mTextView = (TextView) itemView.findViewById(R.id.tile_title);
+            mTextView = (TextView) itemView.findViewById(R.id.tileTitle);
+            mMaxText = (TextView) itemView.findViewById(R.id.tileMax);
+            mImageView = (ImageView) itemView.findViewById(R.id.tileImage);
         }
 
     }
@@ -44,6 +57,9 @@ public class MaxesAdapter extends android.support.v7.widget.RecyclerView.Adapter
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         holder.mTextView.setText(mLifts[position % mLifts.length]);
+        int max = 100 - position;
+        holder.mMaxText.setText(Integer.toString(max) + "kg");
+        holder.mImageView.setImageDrawable(mImages[position % mImages.length]);
     }
 
     @Override
