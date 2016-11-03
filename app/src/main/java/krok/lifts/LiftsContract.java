7936f6f6933f1d1851e8ffadec98d8f73838f681
Tilman Krokotsch <t.krokotsch@gmail.com>
2016-11-03 15:14:37 +0100

    Added date column for maxes; Added view for querying maxes

--- app/src/main/java/krok/lifts/LiftsContract.java
+++ app/src/main/java/krok/lifts/LiftsContract.java
@@ -13,49 +13,58 @@ public final class LiftsContract {
     public static class Lifts implements BaseColumns {
 
         public static final String TABLE_NAME = "lifts";
-        public static final String COLLUMN_NAME_NAME = "name";
-        public static final String COLLUMN_NAME_REPSCHEME = "repscheme";
-        public static final String COLLUMN_NAME_COMPOUND = "compound";
+        public static final String COLUMN_NAME_NAME = "name";
+        public static final String COLUMN_NAME_REPSCHEME = "repscheme";
+        public static final String COLUMN_NAME_COMPOUND = "compound";
 
     }
 
     public static class Maxes implements BaseColumns {
 
         public static final String TABLE_NAME = "maxes";
-        public static final String COLLUMN_NAME_LIFT = "lift";
-        public static final String COLLUMN_NAME_WEIGHT = "weight";
+        public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_LIFT = "lift";
+        public static final String COLUMN_NAME_WEIGHT = "weight";
 
     }
 
     public static class Sets implements BaseColumns {
 
         public static final String TABLE_NAME = "sets";
-        public static final String COLLUMN_NAME_LIFT = "lift";
-        public static final String COLLUMN_NAME_WORKOUT = "workout";
-        public static final String COLLUMN_NAME_ORDER = "ordering";
-        public static final String COLLUMN_NAME_REPS = "reps";
-        public static final String COLLUMN_NAME_WEIGHT = "weight";
+        public static final String COLUMN_NAME_LIFT = "lift";
+        public static final String COLUMN_NAME_WORKOUT = "workout";
+        public static final String COLUMN_NAME_ORDER = "ordering";
+        public static final String COLUMN_NAME_REPS = "reps";
+        public static final String COLUMN_NAME_WEIGHT = "weight";
 
     }
 
     public static class Workouts implements BaseColumns {
 
         public static final String TABLE_NAME = "workouts";
-        public static final String COLLUMN_NAME_DATE = "date";
-        public static final String COLLUMN_NAME_CYCLE = "cycle";
-        public static final String COLLUMN_NAME_WEEK = "week";
-        public static final String COLLUMN_NAME_DONE = "done";
+        public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_CYCLE = "cycle";
+        public static final String COLUMN_NAME_WEEK = "week";
+        public static final String COLUMN_NAME_DONE = "done";
 
     }
 
     public static class Cycles implements BaseColumns {
 
         public static final String TABLE_NAME = "cycles";
-        public static final String COLLUMN_NAME_PRESS = "press";
-        public static final String COLLUMN_NAME_DEAD = "dead";
-        public static final String COLLUMN_NAME_BENCH = "bench";
-        public static final String COLLUMN_NAME_SQUAT = "squat";
+        public static final String COLUMN_NAME_PRESS = "press";
+        public static final String COLUMN_NAME_DEAD = "dead";
+        public static final String COLUMN_NAME_BENCH = "bench";
+        public static final String COLUMN_NAME_SQUAT = "squat";
 
+    }
+
+    public static class MaxesXlifts {
+        public static final String VIEW_NAME = "maxesXlifts";
+        public static final String COLUMN_NAME_DATE = "date";
+        public static final String COLUMN_NAME_LIFT = "lift";
+        public static final String COLUMN_NAME_WEIGHT = "weight";
+        public static final String COLUMN_NAME_LIFTID = "lift_id";
     }
 
 }
